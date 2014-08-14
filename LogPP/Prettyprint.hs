eb64a95883f1078aafb22f9c050dd59bdbd9193c
mathhun <achemer@gmail.com>
2014-08-14 13:16:03 +0900

    WIP: prototyping

--- LogPP/Prettyprint.hs
+++ LogPP/Prettyprint.hs
@@ -1,10 +1,57 @@
 {-# LANGUAGE OverloadedStrings #-}
-module LogPP.Prettyprint where
+module LogPP.Prettyprint (
+    Parser(..)
+  , parseOnly
+  , pp
+  , ppT
+  , parseTime
+) where
 
-import Data.Text as T
+--import Data.Attoparsec.Text
+import Data.Attoparsec.Text.Parsec
+import Data.Map (Map)
+import qualified Data.Map as Map
+import Data.Text (Text)
+import qualified Data.Text as T
+
+data LogEntry = LogEntry
+  { logTime  :: Text
+  , logTable :: Text
+  , logText  :: Text
+  , logBinding :: Map Text Text
+  }
 
 pp :: String -> String
 pp = undefined
 
 ppT :: Text -> Text
 ppT = undefined
+
+parseLog :: Parser LogEntry
+parseLog = do
+  time <- parseTime
+  -- skip log level
+  table <- undefined
+  -- skip elapsed
+  text <- undefined
+  -- semicolon
+  binding <- parseBinding
+  return $ LogEntry time table text binding
+
+parseTime :: Parser Text
+parseTime = do
+  y <- count 4 digit
+  char '-'
+  m <- count 2 digit
+  char '-'
+  d <- count 2 digit
+  char 'T'
+  h <- count 2 digit
+  char ':'
+  mi <- count 2 digit
+  char ':'
+  s <- count 2 digit
+  return $ T.pack $ concat [y, "-", m, "-", d, " ", h, ":", mi, ":", s]
+
+parseBinding :: Parser (Map Text Text)
+parseBinding = undefined
--- tests/PrettyprintSpec.hs
+++ tests/PrettyprintSpec.hs
@@ -1,9 +1,22 @@
+{-# LANGUAGE OverloadedStrings #-}
 module PrettyprintSpec where
 
+import Data.Text
 import Test.Hspec
+
+import LogPP.Prettyprint
+
+log0 :: Text
+log0 = "2014-08-13T18:26:34+09:00 TRACE (8): [dm_common_master](0.00013) SELECT * FROM device WHERE id = :id  ; // bind=>{\":id\":\"sp\"}"
+
+p :: Parser Text -> Either String Text
+p parser = parseOnly parser log0
 
 spec :: Spec
 spec = do
-  describe "sample" $ do
-    it "a test" $
-      "test" `shouldBe` "test"
+  describe "parseTime" $ do
+    it "parses time" $
+      p parseTime `shouldBe` Right "2014-08-13 18:26:34"
+
+main :: IO ()
+main = hspec spec
