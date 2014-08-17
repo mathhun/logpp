8fcf28d3e1982a4c03afb33162e90cd97a6f888b
mathhun <achemer@gmail.com>
2014-08-18 08:13:11 +0900

    WIP

--- tests/PrettyprintSpec.hs
+++ tests/PrettyprintSpec.hs
@@ -7,7 +7,7 @@ import Test.Hspec
 import LogPP.Prettyprint
 
 log0 :: Text
-log0 = "2014-08-13T18:26:34+09:00 TRACE (8): [dm_common_master](0.00013) SELECT * FROM device WHERE id = :id  ; // bind=>{\":id\":\"sp\"}"
+log0 = "2014-08-13T18:26:34+09:00 TRACE (8): [masterdb](0.00013) SELECT * FROM device WHERE id = :id  ; // bind=>{\":id\":\"sp\"}"
 
 p :: Parser Text -> Either String Text
 p parser = parseOnly parser log0
@@ -19,7 +19,7 @@ spec = do
       p parseTime `shouldBe` Right "2014-08-13T18:26:34+09:00"
   describe "parseTable" $ do
     it "parses table name" $
-      parseOnly parseTable "TRACE (8): [dm_common_master](0.00013) SELECT" `shouldBe` Right "dm_common_master"
+      parseOnly parseTable "TRACE (8): [masterdb](0.00013) SELECT" `shouldBe` Right "dm_common_master"
 
 main :: IO ()
 main = hspec spec
